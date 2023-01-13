c291d0b7ab7ea0dc27c1a7e14105af2ef5f8b27c
Alex Evangelou <35777043+AJevangelou@users.noreply.github.com>
2023-01-13 16:14:59 +0200

    working button

--- ticket-frontend/src/root/shared/ErrorPage.tsx
+++ ticket-frontend/src/root/shared/ErrorPage.tsx
@@ -1,6 +1,7 @@
 import { Stack, Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import ErrorSvg from './horror_404.svg'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
@@ -9,7 +10,7 @@ const ErrorPage = () => {
     <Typography variant='h2'>Μη φοβάσαι!</Typography>
     <Typography variant='h4'>Είναι απλά ένα 404!</Typography>
     <img style={{width:'50vw', height:'50vh'}} src={ErrorSvg}/>
-    <Button variant='contained'
+    <Button variant='contained' component={Link} to={'/'}
             sx={{ borderRadius: 15, height: 40, width: '60%', marginTop:1, marginBottom:1, justifyContent:'flex' }} >
                 <Typography variant='subtitle2' fontWeight='normal' textTransform={'none'}>Πίσω στην αρχική</Typography>
         </Button>
